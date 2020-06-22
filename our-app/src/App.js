ddc61dd6128dba848c15f409041a050a9374c4d8
Hewr Srood <hewr_s@outlook.com>
2020-06-22 19:29:04 +0000

    test-1

---
+++ our-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './comps/navbar.js';
+function App() {
+  return (
+   <Navbar></Navbar>
+  );
+}
+
+export default App;
